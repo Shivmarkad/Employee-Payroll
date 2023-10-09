9115b6bb11eb16c9c05ece47167ec65e95da0929
Shivganesh <shivmarkad99@gmail.com>
2023-10-09 12:24:31 +0530

    [Shivganesh] Added: creating jwt token for user login

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,6 +1,6 @@
 import sequelize, { DataTypes } from '../config/database';
 import { IUser } from '../interfaces/user.interface';
-
+import  jwt  from 'jsonwebtoken';
 import user from '../models/user';
 
 class UserService {
@@ -12,6 +12,14 @@ class UserService {
     if(user) throw new Error("User already registered !!")
     const data = await this.User.create(body);
     return data;
+  };
+
+  //Login user
+  public signIn = async (body) => {
+    const data = await this.User.findOne({where: {email: body.email}});
+    if(!data) throw new Error("User not Found !!")
+    const token = jwt.sign({email: data.email, id: data.id},process.env.SECRET_KEY)
+    return token;
   };
 }
 
