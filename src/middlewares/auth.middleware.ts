9fa361fcbdc27ab880060e41fdeefd374f355017
Shivganesh <shivmarkad99@gmail.com>
2023-10-09 14:54:24 +0530

    [Shivganesh] Refactor: using secret key as env variable

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -2,7 +2,8 @@
 import HttpStatus from 'http-status-codes';
 import jwt from 'jsonwebtoken';
 import { Request, Response, NextFunction } from 'express';
-
+import dotenv from 'dotenv';
+dotenv.config();
 /**
  * Middleware to authenticate if user has a valid Authorization token
  * Authorization: Bearer <token>
@@ -25,9 +26,7 @@ export const userAuth = async (
       };
     bearerToken = bearerToken.split(' ')[1];
 
-    const { user }: any = await jwt.verify(bearerToken, 'your-secret-key');
-    res.locals.user = user;
-    res.locals.token = bearerToken;
+    const user : any = await jwt.verify(bearerToken, process.env.SECRET_KEY);
     next();
   } catch (error) {
     next(error);
